d4ef59543b42d809850e83c74c17693081e005db
Lidiya <lesnikoval@yandex.ru>
2023-01-17 23:43:58 +0300

    Задача 15

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+string[] array = num.ToCharArray();
+//string[] array = new Convert.ToString(num);
+if (array.Length<3) Console.WriteLine($"{num} -> третьей цифры нет");
+else Console.WriteLine($"{num} -> {array[2]}");
--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+string PrintNumber(int number)
+{   
+    string rezult;
+    if (number==7||number==6) rezult = "да - это выходной";
+    else rezult = "нет - это рабочий день";
+    return rezult;
+}
+
+Console.Write("Введите номер для недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>0&&num<8) Console.WriteLine($"{num} -> {PrintNumber(num)}");
+else Console.WriteLine("Номер дня недели только от 1 до 7 (((");
