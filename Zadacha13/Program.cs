652b0279bee6c51da228ed7f338f3bc8a8071357
Lidiya <lesnikoval@yandex.ru>
2023-01-18 19:33:18 +0300

    Задача 13 добавлено отрицательное число

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -6,5 +6,7 @@
 
 Console.Write("Введите число: ");
 string num = Console.ReadLine()!;
-if (num.Length<3) Console.WriteLine($"{num} -> третьей цифры нет");
-else Console.WriteLine($"{num} -> третья цифра: {num[2]}");
+int priznak = 3; //положительное
+if (Convert.ToInt32(num)<0) priznak = 4; //отрицательное
+if (num.Length<priznak) Console.WriteLine($"{num} -> третьей цифры нет");
+else Console.WriteLine($"{num} -> третья цифра: {num[priznak-1]}");
